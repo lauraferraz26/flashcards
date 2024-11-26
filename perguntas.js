f73a4b627fe2373d116d14fc131dab7261eb22f6
lauraferraz26 <00001095613649SP@al.educacao.sp.gov.br>
2024-11-26 15:31:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 3 fatorial?',
+    '3 fatorial é 6'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Líbano?',
+    'A capital do Líbano é Beirute'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 3 elevado a 3?',
+    '3 elevado a 3 é 27'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz esperto em Inglês?',
+    'esperto em inglês é smart'
+)
